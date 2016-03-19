9db35d07e9ff19ec2883265481b86ee20d9c3859
Alexander091 <alexander.alekseev091@gmail.com>
2016-03-19 16:08:34 +0300

    CRUD Reception

--- src/api/reception/reception.controller.js
+++ src/api/reception/reception.controller.js
@@ -3,37 +3,109 @@
  */
 'use strict';
 
-var _ = require('lodash');
+//var _ = require('lodash');
 
 var Logger = require('../../services/logger.service');
-var Task = require('./task.model');
+var Reception = require('./reception.model');
 
 var controller = {
   index: index,
-  detail:detail,
-  createReception:createReception,
+  detail: detail,
+  createReception: createReception,
   deleteReception: deleteReception,
-  updateReseption:updateReseption
+  updateReseption: updateReseption
 };
 
 module.exports = controller;
 
-function index(req, res){
+function index (req, res) {
+  var start = new Date().getTime();
+  Reception
+    .find({user: req.user})
+    .skip(req.query.offset)
+    .limit(req.query.max)
+    .sort('name')
+    .then(function (receptions) {
+      res.json(_getFilteredAndLimitedReception(req, receptions));
+    })
+    .catch(function (err) {
+      res.status(400).send(err);
+    })
+    .finally(function () {
+      Logger.debug('ReceptionController::index() - timing', {
+        time: new Date().getTime() - start
+      });
+    });
+}
 
-};
+function detail (req, res) {
+  var start = new Date().getTime();
+  Reception
+    .findOne({id: req.query.receptionId})
+    .then(function (reception) {
+      res.json(reception);
+    })
+    .catch(function (err) {
+      res.status(400).send(err);
+    })
+    .finally(function () {
+      Logger.debug('ReceptionController::index() - timing', {
+        time: new Date().getTime() - start
+      });
+    });
+}
 
-function detail(req, res){
+function createReception (req, res) {
+  var reception = new Reception({
+    time: req.body.time,
+    user: req.user
+  });
+  reception.save(function (err) {
+    if (err) {
+      return res.status(500).send('Uoops! ' + err);
+    }
+    // saved!
+    return res.status(200);
+  });
+}
 
-};
+function deleteReception (req, res) {
+  Reception.remove({id: req.query.reseptionId}, function (err) {
+    if (err) {
+      return res.status(500).send('Uoops! ' + err);
+    }
+    // removed!
+    return res.status(200);
+  });
+}
 
-function creatReception(req, res){
+function updateReseption (req, res) {
+  return res.status(200);
+}
 
-};
 
-function deleteReception(req, res){
+function _getFilteredAndLimitedReception (req, receptions) {
+  var filteredReception = [];
 
-};
+  filteredReception = receptions;
 
-function updateReseption(req, res){
+  var offset = parseInt(req.query.offset) || 0;
+  var limit = 10;
+  filteredReception = filteredReception.slice(offset, offset + limit);
 
-};
+  return {
+    meta: {
+      paging: {
+        total: filteredReception.length,
+        amount: filteredReception.length,
+        offset: offset
+      },
+      filter: {
+        stream: req.query.stream,
+        min: req.query.min,
+        max: req.query.max
+      }
+    },
+    data: filteredReception
+  };
+}
--- src/api/reception/reception.model.js
+++ src/api/reception/reception.model.js
@@ -1,3 +1,19 @@
 /**
  * Created by alexander on 19.03.16.
  */
+'use strict';
+
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+
+var ReceptionSchema = new Schema({
+  id: Number,
+  time: Date,
+  user: Number,
+  schedule: Number,
+  doctor: Number
+});
+
+module.exports = mongoose.model('Reception', ReceptionSchema);

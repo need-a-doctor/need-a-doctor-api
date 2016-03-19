7bf61ad9185c6306e6f3baa6624aa1e6b34b9c4f
Alexander091 <alexander.alekseev091@gmail.com>
2016-03-19 16:03:30 +0300

    CRUD reseption

---
+++ src/api/reception/reception.controller.js
@@ -0,0 +1,39 @@
+/**
+ * Created by alexander on 19.03.16.
+ */
+'use strict';
+
+var _ = require('lodash');
+
+var Logger = require('../../services/logger.service');
+var Task = require('./task.model');
+
+var controller = {
+  index: index,
+  detail:detail,
+  createReception:createReception,
+  deleteReception: deleteReception,
+  updateReseption:updateReseption
+};
+
+module.exports = controller;
+
+function index(req, res){
+
+};
+
+function detail(req, res){
+
+};
+
+function creatReception(req, res){
+
+};
+
+function deleteReception(req, res){
+
+};
+
+function updateReseption(req, res){
+
+};
---
+++ src/api/reception/reception.model.js
@@ -0,0 +1,3 @@
+/**
+ * Created by alexander on 19.03.16.
+ */
